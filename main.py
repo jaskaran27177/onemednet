f2643d1c1a23c7c6e811b2a3d11b3cb0a87e8593
jaskaran27177 <jaskarandhanoa7@gmail.com>
2025-08-27 10:50:05 -0700

    removed useless comment.

--- main.py
+++ main.py
@@ -34,5 +34,5 @@ def hello_world():
 if __name__ == '__main__':
 
     import os
-    port = int(os.environ.get('PORT', 8080))  # Changed default port to 8081
+    port = int(os.environ.get('PORT', 8080))  
     app.run(host='0.0.0.0', port=port)
