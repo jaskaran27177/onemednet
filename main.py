ea388c4c2db7f182f6fc1130e4b6a6e8d644c4ff
jaskaran27177 <jaskarandhanoa7@gmail.com>
2025-08-27 10:18:41 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify
+import time
+import uuid
+from datetime import datetime, timezone
+import pytz
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """
+    This is a Simple Get request for Steven. 
+    """
+    unique_id = uuid.uuid4()
+
+    current_utc_time = datetime.now(timezone.utc)
+    
+    # Define the Vancouver timezone.
+    vancouver_timezone = pytz.timezone('America/Vancouver')
+    
+    # Convert the UTC time to Vancouver time.
+    current_vancouver_time = current_utc_time.astimezone(vancouver_timezone)
+
+    output=jsonify({
+        "id": str(unique_id), 
+        "timestamp": current_utc_time.strftime("%a, %d %b %Y %H:%M:%S GMT"),
+        "vancouver_time": current_vancouver_time.strftime("%a, %d %b %Y %H:%M:%S %Z")
+        })
+    return output
+
+    
+
+if __name__ == '__main__':
+
+    import os
+    port = int(os.environ.get('PORT', 8080))  # Changed default port to 8081
+    app.run(host='0.0.0.0', port=port)
